f7c508a07ffc3813533ba4805d08531fc4467665
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-29 18:11:45 -0500

    feat: add links to change pages

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Navigation() {
-  return <nav></nav>;
+  return <nav>
+    <Link to="/">Home</Link>
+    <Link to="shop">Shop</Link>
+  </nav>;
 }
 
 export default Navigation;
