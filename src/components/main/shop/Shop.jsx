888fd28cf1b75b87efe6d35431ba023e9934ef83
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-29 18:57:33 -0500

    feat: fetch products data from FakeStoreAPI

--- src/components/main/shop/Shop.jsx
+++ src/components/main/shop/Shop.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import Product from "./Product";
 import Cart from "./Cart";
 
 function Shop() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/")
+      .then((response) => response.json())
+      .then((json) => {
+        const newProducts = [];
+        json.forEach((item) => {
+          const product = {
+            id: item.id,
+            title: item.title,
+            price: item.price,
+            category: item.category,
+            image: item.image,
+          };
+          newProducts.push(product);
+        });
+        setProducts(newProducts);
+      });
+  }, []);
   return (
     <div id="shop">
       <Product />
