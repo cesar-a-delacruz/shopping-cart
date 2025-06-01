5f6e9b0f982ce5ad045cfce1723c96bff078dca1
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-01 03:09:38 -0500

    test: check Products rendering in Shop

---
+++ tests/Shop.test.jsx
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { render, waitFor, screen } from "@testing-library/react";
+import * as Routing from "../src/Routing";
+import App from "../src/App";
+
+Routing.setRouting(<App />);
+
+describe("render content", () => {
+  it("products", async () => {
+    const memRouter = createMemoryRouter(Routing.routes, {
+      initialEntries: ["/shop"],
+    });
+
+    render(<RouterProvider router={memRouter} />);
+
+    await waitFor(() => {
+      expect(document.querySelectorAll("div.product").length).toBe(20);
+    });
+  });
+});
