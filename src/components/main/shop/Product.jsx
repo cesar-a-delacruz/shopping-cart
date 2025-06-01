9b98fe4daa4470d6b0d92e602932873e99e6776f
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-01 03:05:42 -0500

    feat: get data for Product.jsx content

--- src/components/main/shop/Product.jsx
+++ src/components/main/shop/Product.jsx
@@ -1,5 +1,13 @@
-function Product() {
-  return <div className="product"></div>;
+function Product({ data }) {
+  return (
+    <div className="product">
+      <img src={data.image} alt={data.title} />
+      <p>{data.title}</p>
+      <p>{data.category}</p>
+      <p>{data.price}</p>
+      <p>{data.price}</p>
+    </div>
+  );
 }
 
 export default Product;
