75a62afd93ab1faf648286c5ae276d09ff70b271
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-03 01:48:11 -0500

    feat: add content to Cart and handle open and exit

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -5,8 +5,8 @@ function Navigation() {
     <nav>
       <Link to="/">Home</Link>
       <Link to="shop">Shop</Link>
-      <button>
-        Cart <span>(0)</span>
+      <button onClick={() => document.querySelector("dialog#cart").showModal()}>
+        Cart<span>(0)</span>
       </button>
     </nav>
   );
--- src/components/main/shop/Cart.jsx
+++ src/components/main/shop/Cart.jsx
@@ -1,5 +1,14 @@
 function Cart() {
-  return <dialog id="cart"></dialog>;
+  return (
+    <dialog id="cart">
+      <button onClick={(e) => e.currentTarget.parentElement.close()}>X</button>
+      <div>
+        <p>
+          Total:<span>$0.00</span>
+        </p>
+      </div>
+    </dialog>
+  );
 }
 
 export default Cart;
