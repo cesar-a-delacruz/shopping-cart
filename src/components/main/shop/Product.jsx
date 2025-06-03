5fc771bd897bd4b2baa1d191168f0892e8f5493f
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-03 00:48:21 -0500

    feat: add form for amount of products for the cart

--- src/components/main/shop/Product.jsx
+++ src/components/main/shop/Product.jsx
@@ -2,10 +2,16 @@ function Product({ data }) {
   return (
     <div className="product">
       <img src={data.image} alt={data.title} />
+
       <p>{data.title}</p>
       <p>{data.category}</p>
       <p>{data.price}</p>
-      <p>{data.price}</p>
+
+      <form method="post">
+        <label htmlFor="amount">Amount:</label>
+        <input type="number" name="" id="amount" placeholder="1" min={1} />
+        <button type="submit">Add to Cart</button>
+      </form>
     </div>
   );
 }
