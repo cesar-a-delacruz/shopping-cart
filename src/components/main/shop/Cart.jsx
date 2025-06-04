5b065a43def8f5da88dbbb313db6032abb82cd21
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-04 01:00:17 -0500

    feat: fill Cart with list of products

--- src/components/main/shop/Cart.jsx
+++ src/components/main/shop/Cart.jsx
@@ -1,12 +1,24 @@
-function Cart() {
+function Cart({ data }) {
+  let total = 0;
   return (
     <dialog id="cart">
       <button onClick={(e) => e.currentTarget.parentElement.close()}>X</button>
-      <div>
-        <p>
-          Total:<span>$0.00</span>
-        </p>
-      </div>
+      <ul>
+      {data.map((value) => {
+        const subtotal = value.price * value.amount;
+        total = total + subtotal;
+        return <li key={value.id}>
+          <img src={value.image} alt="" />
+          <p>Title: <span>{value.title}</span></p>
+          <p>Amount: <span>{value.amount}</span></p>
+          <p>Price: <span>${value.price}</span></p>
+          <p>Subtotal: <span>${subtotal}</span></p>
+        </li>
+      })}
+      </ul>
+      <p>
+        Total:<span>${total.toFixed(2)}</span>
+      </p>
     </dialog>
   );
 }
