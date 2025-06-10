e8476a86f855d16172547edd597cd0f28bd57b75
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-10 10:53:57 -0500

    feat: show cart button if current path is /shop

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,17 @@
+import { useLocation } from "react-router-dom";
 import Navigation from "./Navigation";
 
 function Header() {
+  const butCart = (
+    <button onClick={() => document.querySelector("dialog#cart").showModal()}>
+      Cart<span></span>
+    </button>
+  );
+  const currrenPath = useLocation().pathname;
   return (
     <header>
       <Navigation />
-      <button onClick={() => document.querySelector("dialog#cart").showModal()}>
-        Cart<span></span>
-      </button>
+      {currrenPath === "/shop" && butCart}
     </header>
   );
 }
