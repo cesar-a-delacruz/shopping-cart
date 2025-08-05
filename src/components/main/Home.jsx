fdc99bcd153abb843ff740b6b40924ebcd74e1e8
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-08-04 20:29:56 -0500

    feat: style home page

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,5 +1,15 @@
+import "../../styles/main/Home.css";
+
 function Home() {
-  return <div id="home"></div>;
+  return (
+    <div id="home">
+      <h1>
+        Discover handpicked treasures, unbeatable deals, and effortless
+        shopping. <br /> All in one place.
+      </h1>
+      <a href="/shop">Explore Shop</a>
+    </div>
+  );
 }
 
 export default Home;
